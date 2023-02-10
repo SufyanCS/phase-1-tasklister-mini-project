a9e7df1a86e7c4d63d152f21e3b55fa7751d88b8
SufyanCS <sufianb.contact@gmail.com>
2023-02-10 17:54:25 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById('create-task-form')
+  const todosList = document.getElementById('tasks')
+  taskForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const taskInput = document.getElementById('new-task-description').value
+    todosList.innerHTML += `<li> ${taskInput} </li>`
+  })
 });
